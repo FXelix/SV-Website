1c2b03b895dbb0318765687417ca090fa4e8d443
FXelix <felix.scheling@web.de>
2016-09-06 16:31:51 +0200

    HTML angefangen
    Grundlegende Ansätze von:
    - Header
    - Navbar
    - Showcase der Artikel

--- index.php
+++ index.php
@@ -1,17 +1,42 @@
-<!-- php -->
-
-
 <!DOCTYPE html>
 <html>
-	<head>
-		<title>SV Shop</title>
-		<meta charset="UTF-8">
-		<link rel="stylesheet" type="text/css" href="style.css">
-	</head>
+<head>
+	<title>Test-Website</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<div class="header">
+<h1>HAPPY PEOPLE</h1>
 	
-	<body>
-		
-		<h1>Vikilu SV Shop</h1>
+</div>
 	
-	</body>
+<nav class="menu">
+	<ul>
+		<li><a href="" title=""></a>Pullis</li>
+		<li><a href="" title=""></a>Polos</li>
+		<li><a href="" title=""></a>Kulis</li>
+		<li><a href="" title=""></a>Bleistifte</li>
+
+	</ul>
+</nav>
+<div class="content">
+	<div class="col-12">
+		<h1>Pullis</h1>
+		<p>hello test test</p>
+	</div>
+	<div class="col-12">
+		<h1>Polos</h1>
+		<p>hello test test</p>	
+	</div>	
+	<div class="col-12">
+		<h1>Bleistifte</h1>
+		<p>hello test test</p>	
+	</div>
+	<div class="col-12">
+		<h1>Kulis</h1>
+		<p>hello test test</p>	
+	</div>	
+</div>
+	
+</body>
 </html>
