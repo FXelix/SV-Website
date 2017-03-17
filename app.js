9124650374ee9c69162a7ed5d573d0cdfd6b9bc5
FXelix <felix.scheling@web.de>
2017-03-17 14:46:07 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -7,3 +7,6 @@
     $(function(){
         $("#copyright").load("/SV-Website/standard/copyright.html");
     });
+    $(function(){
+        $("#bestellinfo").load("/SV-Website/standard/bestellinfo.html");
+    });
